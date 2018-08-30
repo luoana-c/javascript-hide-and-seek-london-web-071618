f2022d6527f9ead25dc3b3a514770dadff481629
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-30 13:47:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  list = document.querySelector(".ranked-list li")
+  list = document.getElementsBy(".ranked-list li")
   for (i = 0; i < list.length; i++){
      list[i].innerHTML = (list[i].parseInt() + 1).toString()
   }
