f1d3a61e9eacf6b2e0a1755eae0f45436ab1eec4
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-30 13:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   list = document.querySelectorAll(".ranked-list")
   for (i = 0; i < list.length; i++){
     let children = list[i].children
-     for (i = 0; i < list.length; i++){
+     for (j = 0; j < children.length; j++){
       list[i].innerHTML = (list[i].parseInt() + 1).toString()
   }
 }
