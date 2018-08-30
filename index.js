b87bef571f8f4b24e0b62ae583cc74ade9c96af6
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-30 13:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n) {
+  list = document.querySelectorAll(".ranked-list")
+  for (i = 0; i < list.length; i++){
+    let children = list[i].children
+    
+     for (j = 0; j < children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+     }
+  }
+}
+
+function deepestChild(){
+  let node = document.getElementById("grand-node")
+  let nextNode = node.children[0]
+  
+  while (nextNode) {
+    node = nextNode 
+    nextNode = node.children[0]
+  }
+  
+  return node
+}
